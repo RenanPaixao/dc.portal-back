8cca0ed7acb95827eb676efe4aa617030674d0bf
renan <renanlemos21@gmail.com>
2024-10-07 11:16:08 -0300

    feat: handle context error and click on tab

--- src/scrap/index.ts
+++ src/scrap/index.ts
@@ -1,5 +1,44 @@
 import puppeteer from 'puppeteer-core'
 
+async function clickOnCourseTab(page: puppeteer.Page, tabName: string) {
+  await page.waitForSelector('.yui-nav')
+  const tabs = await page.$$('.yui-nav a')
+
+  for (const tab of tabs) {
+    const text = await tab.getProperty('innerText')
+    const value = await text.jsonValue()
+    if (value === tabName) {
+      await tab.click()
+    }
+  }
+}
+
+async function clickOnActiveRow(page: puppeteer.Page) {
+  const rows = await page.$$('tr')
+
+  try {
+    for (const row of rows) {
+      const text = await row.getProperty('innerText')
+      const value = await text.jsonValue()
+      if (value.includes('Ativa')) {
+        const cell = await row.$('[title="Visualizar Estrutura Curricular"]')
+        if (cell) {
+          await cell.click()
+        }
+      }
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      if (e.message === 'Protocol error (DOM.describeNode): Cannot find context with specified id') {
+        console.error(e.message)
+        return
+      }
+    }
+
+    throw e
+  }
+}
+
 function getBrowser() {
   return puppeteer.launch({
     executablePath: '/usr/bin/google-chrome-stable',
@@ -44,18 +83,8 @@ async function scrapCourses() {
   await page.goto('https://sigs.ufrpe.br/sigaa/public/curso/curriculo.jsf?lc=pt_BR&id=28230822')
 
   await page.waitForSelector('tr')
-  const rows = await page.$$('tr')
-
-  for (const row of rows) {
-    const text = await row.getProperty('innerText')
-    const value = await text.jsonValue()
-    if (value.includes('Ativa')) {
-      const cell = await row.$('[title="Visualizar Estrutura Curricular"]')
-      if (cell) {
-        await cell.click()
-      }
-    }
-  }
+  clickOnActiveRow(page).catch(console.error)
+  await clickOnCourseTab(page, 'Optativas')
 
   return []
 }
@@ -67,4 +96,4 @@ async function main() {
 main()
   .then(() => console.log('Done!'))
   .catch(console.error)
-  .finally(() => process.exit(0))
+// .finally(() => process.exit(0))
